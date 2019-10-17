88a7f423f61c7966f5a7d4611b00057a742b2129
rodolpheh <houdas.rodolphe@gmail.com>
2019-10-17 09:03:49 +0200

    Fixing wrong bash comparison

--- build-example.sh
+++ build-example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $3 == "debug" ]; then
+if [ "$3" == "debug" ]; then
     pio ci -c configs/platformio-example-debug.ini --build-dir /tmp/piobuild --keep-build-dir -l lib/$1/ lib/$1/examples/$2.cpp
 else
     pio ci -c configs/platformio-example.ini --build-dir /tmp/piobuild --keep-build-dir -l lib/$1/ lib/$1/examples/$2.cpp
