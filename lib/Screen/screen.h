05a9eeed3bdee3911ce06c68c6d0132cb194bfce
Hexliath <admin@example.com>
2019-10-21 11:45:44 +0200

    Add screen.h

--- lib/Screen/screen.h
+++ lib/Screen/screen.h
@@ -16,11 +16,17 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define NUMFLAKES     10 // Number of snowflakes in the animation example
 
  
-void init();
-void update_values(int *values);
-void draw_heart();
-void draw_coin();
-void set_header(char *str);
-void print_string(char* str, int x, int y);
+void init(); // Clear and test the screen
+void score(char* score); //Display score
+void draw_heart(); // Draw heart BMP
+void draw_coin(); // Draw coin BMP  
+void set_header(char *str); // Set the header (yellow color)
+void print_string(char* str, int x, int y); //Print a string on the screen (str,x, y)
+int parseInt(char* chars);
+int powInt(int x, int y);
+void display_screen_2(char *world, char *life); // Screen when world change or you die
+void display_screen_1(char *lifes, char *coins, char *val_score); // Screen in game
+void draw_head(); // Draw mario head BMP
+
 
 #endif
